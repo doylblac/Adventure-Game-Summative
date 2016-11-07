d8e36c58b700fcaeee2301b7e4f7d37ccf0cbe45
doylblac <doylblac245@ed.amdsb.ca>
2016-11-07 13:35:02 -0500

    Add project files.

---
+++ Adventure Game Summative/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Adventure_Game_Summative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.firstButtonLabel = new System.Windows.Forms.Label();
+            this.middlePictureBox = new System.Windows.Forms.PictureBox();
+            this.redButtonPicture = new System.Windows.Forms.PictureBox();
+            this.blueButtonPicture = new System.Windows.Forms.PictureBox();
+            this.firstChoiceLabel = new System.Windows.Forms.Label();
+            this.yellowButtonPicture = new System.Windows.Forms.PictureBox();
+            this.secondChoiceLabel = new System.Windows.Forms.Label();
+            this.thirdChoiceLabel = new System.Windows.Forms.Label();
+            this.beginButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.middlePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redButtonPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueButtonPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowButtonPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(13, 13);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(344, 73);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Welcome adventurer, what is your name? ";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(297, 14);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(60, 20);
+            this.nameInput.TabIndex = 1;
+            // 
+            // firstButtonLabel
+            // 
+            this.firstButtonLabel.AutoSize = true;
+            this.firstButtonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstButtonLabel.ForeColor = System.Drawing.Color.White;
+            this.firstButtonLabel.Location = new System.Drawing.Point(13, 90);
+            this.firstButtonLabel.Name = "firstButtonLabel";
+            this.firstButtonLabel.Size = new System.Drawing.Size(170, 18);
+            this.firstButtonLabel.TabIndex = 2;
+            this.firstButtonLabel.Text = "Click the button to begin.";
+            // 
+            // middlePictureBox
+            // 
+            this.middlePictureBox.Location = new System.Drawing.Point(16, 127);
+            this.middlePictureBox.Name = "middlePictureBox";
+            this.middlePictureBox.Size = new System.Drawing.Size(341, 122);
+            this.middlePictureBox.TabIndex = 4;
+            this.middlePictureBox.TabStop = false;
+            // 
+            // redButtonPicture
+            // 
+            this.redButtonPicture.BackColor = System.Drawing.Color.Red;
+            this.redButtonPicture.Location = new System.Drawing.Point(16, 255);
+            this.redButtonPicture.Name = "redButtonPicture";
+            this.redButtonPicture.Size = new System.Drawing.Size(59, 44);
+            this.redButtonPicture.TabIndex = 5;
+            this.redButtonPicture.TabStop = false;
+            // 
+            // blueButtonPicture
+            // 
+            this.blueButtonPicture.BackColor = System.Drawing.Color.Blue;
+            this.blueButtonPicture.Location = new System.Drawing.Point(16, 305);
+            this.blueButtonPicture.Name = "blueButtonPicture";
+            this.blueButtonPicture.Size = new System.Drawing.Size(59, 42);
+            this.blueButtonPicture.TabIndex = 6;
+            this.blueButtonPicture.TabStop = false;
+            // 
+            // firstChoiceLabel
+            // 
+            this.firstChoiceLabel.ForeColor = System.Drawing.Color.White;
+            this.firstChoiceLabel.Location = new System.Drawing.Point(82, 255);
+            this.firstChoiceLabel.Name = "firstChoiceLabel";
+            this.firstChoiceLabel.Size = new System.Drawing.Size(275, 44);
+            this.firstChoiceLabel.TabIndex = 7;
+            // 
+            // yellowButtonPicture
+            // 
+            this.yellowButtonPicture.BackColor = System.Drawing.Color.Yellow;
+            this.yellowButtonPicture.Location = new System.Drawing.Point(16, 353);
+            this.yellowButtonPicture.Name = "yellowButtonPicture";
+            this.yellowButtonPicture.Size = new System.Drawing.Size(59, 42);
+            this.yellowButtonPicture.TabIndex = 8;
+            this.yellowButtonPicture.TabStop = false;
+            // 
+            // secondChoiceLabel
+            // 
+            this.secondChoiceLabel.ForeColor = System.Drawing.Color.White;
+            this.secondChoiceLabel.Location = new System.Drawing.Point(82, 303);
+            this.secondChoiceLabel.Name = "secondChoiceLabel";
+            this.secondChoiceLabel.Size = new System.Drawing.Size(275, 44);
+            this.secondChoiceLabel.TabIndex = 9;
+            // 
+            // thirdChoiceLabel
+            // 
+            this.thirdChoiceLabel.ForeColor = System.Drawing.Color.White;
+            this.thirdChoiceLabel.Location = new System.Drawing.Point(82, 351);
+            this.thirdChoiceLabel.Name = "thirdChoiceLabel";
+            this.thirdChoiceLabel.Size = new System.Drawing.Size(275, 44);
+            this.thirdChoiceLabel.TabIndex = 10;
+            // 
+            // beginButton
+            // 
+            this.beginButton.Location = new System.Drawing.Point(297, 90);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(59, 23);
+            this.beginButton.TabIndex = 11;
+            this.beginButton.Text = "Begin";
+            this.beginButton.UseVisualStyleBackColor = true;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(369, 417);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.thirdChoiceLabel);
+            this.Controls.Add(this.secondChoiceLabel);
+            this.Controls.Add(this.yellowButtonPicture);
+            this.Controls.Add(this.firstChoiceLabel);
+            this.Controls.Add(this.blueButtonPicture);
+            this.Controls.Add(this.redButtonPicture);
+            this.Controls.Add(this.middlePictureBox);
+            this.Controls.Add(this.firstButtonLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.middlePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redButtonPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueButtonPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowButtonPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label firstButtonLabel;
+        private System.Windows.Forms.PictureBox middlePictureBox;
+        private System.Windows.Forms.PictureBox redButtonPicture;
+        private System.Windows.Forms.PictureBox blueButtonPicture;
+        private System.Windows.Forms.Label firstChoiceLabel;
+        private System.Windows.Forms.PictureBox yellowButtonPicture;
+        private System.Windows.Forms.Label secondChoiceLabel;
+        private System.Windows.Forms.Label thirdChoiceLabel;
+        private System.Windows.Forms.Button beginButton;
+    }
+}
+
---
+++ Adventure Game Summative/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Adventure_Game_Summative
+{
+    public partial class Form1 : Form
+    {
+        string name;
+        int scene = 0;
+        Random randNum = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            middlePictureBox.Visible = false;
+            redButtonPicture.Visible = false;
+            yellowButtonPicture.Visible = false;
+            blueButtonPicture.Visible = false;
+            firstChoiceLabel.Visible = false;
+            secondChoiceLabel.Visible = false;
+            thirdChoiceLabel.Visible = false;
+        }
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            name = nameInput.Text;
+
+            if (nameInput.Text == "")
+            {
+                titleLabel.Text = "Welcome adventurer, what is your name?\nPlease enter your name.";
+            }
+            else
+            {
+                nameInput.Visible = false;
+                firstButtonLabel.Visible = false;
+                beginButton.Visible = false;
+
+                beginButton.Enabled = false;
+
+                middlePictureBox.Visible = true;
+                redButtonPicture.Visible = true;
+                yellowButtonPicture.Visible = true;
+                blueButtonPicture.Visible = true;
+                firstChoiceLabel.Visible = true;
+                secondChoiceLabel.Visible = true;
+                thirdChoiceLabel.Visible = true;
+
+                titleLabel.Text = name + " looks at his phone. It reads: November 1, 12:00 AM. " + name + " looks around himself and barely makes out an abandoned lab at Central. What should " + name + " do?";
+                firstChoiceLabel.Text = "Check the front door";
+                secondChoiceLabel.Text = "Check the back door";
+                thirdChoiceLabel.Text = "Investigate the lab";
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.R)
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 2)
+                {
+                    scene = 3;
+                }
+                else if (scene == 5)
+                {
+                    scene = 6;
+                }
+                else if (scene == 9)
+                {
+                    scene = 10;
+                }
+                else if (scene == 10)
+                {
+                    scene = 13;
+                }
+                else if (scene == 9)
+                {
+                    scene = 10;
+                }
+                else if (scene == 7)
+                {
+                    scene = 9;
+                }
+                else if (scene == 14)
+                {
+                    scene = 16;
+                }
+                else if (scene == 6)
+                {
+                    scene = 0;
+                }
+                else if (scene == 3)
+                {
+                    scene = 0;
+                }
+                else if (scene == 4)
+                {
+                    scene = 0;
+                }
+                else if (scene == 8)
+                {
+                    scene = 0;
+                }
+                else if (scene == 11)
+                {
+                    scene = 0;
+                }
+                else if (scene == 12)
+                {
+                    scene = 0;
+                }
+                else if (scene == 13)
+                {
+                    scene = 0;
+                }
+                else if (scene == 15)
+                {
+                    scene = 0;
+                }
+            }
+
+            else if (e.KeyCode == Keys.B)
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+                else if (scene == 1)
+                {
+                    scene = 5;
+                }
+                else if (scene == 5)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
+                    scene = 8;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 9)
+                {
+                    scene = 14;
+                }
+                else if (scene == 14)
+                {
+                    scene = 15;
+                }
+                else if (scene == 6)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 3)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 4)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 8)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 11)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 12)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 13)
+                {
+                    Application.Exit();
+                }
+                else if (scene == 15)
+                {
+                    Application.Exit();
+                }
+            }
+
+            else if (e.KeyCode == Keys.Y)
+            {
+                if (scene == 10)
+                {
+                    scene = 11;
+                }
+            }
+
+            switch (scene)
+            {
+                case 0:
+                    titleLabel.Text = name + " looks at his phone. It reads: November 1, 12:00 AM." + name + "looks around himself and barely makes out an abandoned lab at Central. What should" + name + "do?";
+                    firstChoiceLabel.Text = "Check the front door";
+                    secondChoiceLabel.Text = "Check the back door";
+                    thirdChoiceLabel.Text = "Investigate the lab";
+                    break;
+                case 1:
+                    titleLabel.Text = "The door is unlocked, " + name + " heres an eerie rustle behind them. What should " + name + " do?";
+                    firstChoiceLabel.Text = "Run away from the mysterious noise";
+                    secondChoiceLabel.Text = "Turn around and investigate";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 2:
+                    titleLabel.Text = "The door is unlocked, " + name + " hears the Bill Nye theme song behind them. Do you want to.";
+                    firstChoiceLabel.Text = "Sing along";
+                    secondChoiceLabel.Text = "See what is going on";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 3:
+                    titleLabel.Text = name + " was jamming too hard and got killed by the monster.\nIf you want to restart press R. If you want to exit press B.";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 4:
+                    titleLabel.Text = name + " gggis caught by the monster and slaughtered.\nIf you want to restart press R. If you want to exit press B.";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 5:
+                    titleLabel.Text = name + " sees a figure in the darkness and shuts the door then runs down the stairs to the second floor. Should" + name + "enter the library or continue running down the stairs?.";
+                    firstChoiceLabel.Text = "Continue running down the stairs";
+                    secondChoiceLabel.Text = "Enter the Library";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 6:
+                    titleLabel.Text = name + " hhhhhis caught by the monster and killed.\nIf you want to restart press R. If you want to exit press B.";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 7:
+                    titleLabel.Text = name + " enters the library and barricades the door with a table. " + name + "Realizes that there are onky two exits. Which exit should" + name + " take?";
+                    firstChoiceLabel.Text = "Left door";
+                    secondChoiceLabel.Text = "Right door";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 8:
+                    titleLabel.Text = name + "fffff is caught by the monster and killed.\nIf you want to restart press R. If you want to exit press B.";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 9:
+                    titleLabel.Text = "The door is unlocked and " + name + "runs down the stairs to the first floor. Should " + name + "go into the cafeteria or attempt to escape through the back doors.";
+                    firstChoiceLabel.Text = "Cafeteria";
+                    secondChoiceLabel.Text = "Back doors";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 10:
+                    titleLabel.Text = name + " enters the cafeteria and sees that the exits are blocked. What should " + name + " do?";
+                    firstChoiceLabel.Text = "Pray for your life";
+                    secondChoiceLabel.Text = "Break the window";
+                    thirdChoiceLabel.Text = "Fight";
+                    break;
+                case 11:
+                    titleLabel.Text = "The monster throws a chair at " + name + " and knocks them out. " + name + " doesn't know what happened next...";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 12:
+                    titleLabel.Text = "The glass is too thick to break and the monster kills " + name;
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 13:
+                    titleLabel.Text = "The monster laughs at " + name + "'s attempt to survive. The monster kills you.";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 14:
+                    titleLabel.Text = "The back doors are locked. What should  " + name + " do?";
+                    firstChoiceLabel.Text = "Fight the monster";
+                    secondChoiceLabel.Text = "Look for another exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 15:
+                    titleLabel.Text = name + " is caught by the monster and killed.\nIf you want to restart press R. If you want to exit press B.";
+                    firstChoiceLabel.Text = "Restart";
+                    secondChoiceLabel.Text = "Exit";
+                    thirdChoiceLabel.Text = "";
+                    break;
+                case 16:
+                    titleLabel.Text = name + " fights the monster... coming soon.";
+                    firstChoiceLabel.Visible = false;
+                    secondChoiceLabel.Visible = false;
+                    thirdChoiceLabel.Visible = false;
+                    redButtonPicture.Visible = false;
+                    blueButtonPicture.Visible = false;
+                    yellowButtonPicture.Visible = false;
+
+                    Refresh();
+                    Thread.Sleep(1000);
+
+                    Application.Exit();
+                    break;
+                case 17:
+                    break;
+            }
+        }
+
+
+    }
+}
